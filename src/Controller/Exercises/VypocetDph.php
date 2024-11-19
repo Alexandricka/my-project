3bbed613e00440dbf843b3dc88ba47f247531081
Alexandra Kvasnicová <alexkvasnicova4@gmail.com>
2024-11-19 13:26:04 +0100

    improve 2nd exercise

--- src/Controller/Exercises/VypocetDph.php
+++ src/Controller/Exercises/VypocetDph.php
@@ -4,7 +4,7 @@ namespace App\Controller\Exercises;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Response; //hodnoty odoslane formularom http udaje
 use Symfony\Component\Routing\Attribute\Route; 
 
 class VypocetDph extends AbstractController
